22a9dfce759408d4f22c73beb46b01a70dcaf5a8
Attila Bulenda <36601162+Zumwaloro@users.noreply.github.com>
2020-06-01 19:53:08 +0200

    Added tests
    But they are commented out in the Main class so that I don't have to run all of them when testing a new one. They will be uncommented and restructered later.

--- src/no/attila/Main.java
+++ src/no/attila/Main.java
@@ -8,5 +8,55 @@ public class Main {
 
         main.getWebPage("http://the-internet.herokuapp.com/");
 
+        //A/B Testing
+        /*main.getAndClickButtonByLinkText("A/B Testing");
+        main.navigateBack();*/
+
+        //Add/Remove Elements
+        /*main.getAndClickButtonByLinkText("Add/Remove Elements");
+        main.testAddAndRemoveButton("/html/body/div[2]/div/div/button","/html/body/div[2]/div/div/div/button");
+        main.navigateBack();*/
+
+        //Basic Auth
+       // main.getWebPage("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+        // main.navigateBack();
+
+        //Test broken images
+       /* main.getAndClickButtonByLinkText("Broken Images");
+        main.testBrokenImages();
+        main.navigateBack();*/
+
+       //Challenging DOM elements
+        //main.getAndClickButtonByLinkText("Challenging DOM");
+        /*main.getElementByClass("button");
+        main.getElementByMultipleClassName(".button.alert");
+        main.getElementByMultipleClassName(".button.success");
+        main.printTableRowElements("/html/body/div[2]/div/div/div/div/div[2]/table/tbody/tr[1]");
+        main.clickEditeAndDeleteInRow("/html/body/div[2]/div/div/div/div/div[2]/table/tbody/tr[1]/td[7]/a[1]",
+                                    "/html/body/div[2]/div/div/div/div/div[2]/table/tbody/tr[1]/td[7]/a[2]");
+        main.navigateBack();
+        */
+
+        //Checkboxes
+        /*main.getAndClickButtonByLinkText("Checkboxes");
+        main.getAndClickCheckBoxes("//*[@id=\"checkboxes\"]");
+        main.navigateBack();*/
+
+        //Context menu
+        /*main.getAndClickButtonByLinkText("Context Menu");
+        main.selectAndClickContextMenu("hot-spot");
+        main.navigateBack();*/
+
+        //Drag & Drop
+        /*main.getAndClickButtonByLinkText("Drag and Drop");
+        main.dragAndDropElement("column-a", "column-b");
+        main.navigateBack();*/
+
+        //Dropdown
+        /*main.getAndClickButtonByLinkText("Dropdown");
+        main.selectDropdownMenu("dropdown");
+        main.navigateBack();*/
+
+
     }
 }
--- src/no/attila/MainPOM.java
+++ src/no/attila/MainPOM.java
@@ -1,13 +1,101 @@
 package no.attila;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 public class MainPOM {
 
-    DriverSetUp setup = new DriverSetUp();
-    WebDriver driver = setup.getDrvier();
+    private final DriverSetUp setup = new DriverSetUp();
+    private final WebDriver driver = setup.getDrvier();
+    private final Actions actions = new Actions(driver);
 
     public void getWebPage(String url) {
         driver.get(url);
     }
+
+    public void getAndClickButtonByLinkText(String text) {
+        WebElement button = ((ChromeDriver) driver).findElementByLinkText(text);
+        button.click();
+    }
+
+    public void navigateBack() {
+        driver.navigate().back();
+    }
+
+    public void testAddAndRemoveButton(String addPath, String removePath) {
+        WebElement add = driver.findElement(By.xpath(addPath));
+        add.click();
+        WebElement remove = driver.findElement(By.xpath(removePath));
+        remove.click();
+    }
+
+    public void testBrokenImages() {
+        List<WebElement> images = driver.findElements(By.tagName("img"));
+        for (WebElement e : images) {
+            try {
+                URL url = new URL(e.getAttribute("src"));
+                HttpURLConnection http = (HttpURLConnection)url.openConnection();
+                if (http.getResponseCode() != 200) {
+                    System.out.println("Invalid image with src: " + e.getAttribute("src"));
+                } else {
+                    System.out.println("Valid image with source: " + e.getAttribute("src"));
+                }
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    public void getElementByClass(String className) {
+        WebElement button = driver.findElement(By.className(className));
+        button.click();
+    }
+
+    public void getElementByMultipleClassName(String multiClass) {
+        driver.findElement(By.cssSelector(multiClass)).click();
+    }
+
+    public void printTableRowElements(String rowPath) {
+        List<WebElement> tableElements = driver.findElement(By.xpath(rowPath)).findElements(By.tagName("td"));
+        for (WebElement e : tableElements) {
+            System.out.println(e.getText());
+        }
+    }
+
+    public void clickEditeAndDeleteInRow(String edit, String delete) {
+        driver.findElement(By.xpath(edit)).click();
+        driver.findElement(By.xpath(delete)).click();
+    }
+
+    public void getAndClickCheckBoxes(String containerPath) {
+        List<WebElement> elements = driver.findElement(By.xpath(containerPath)).findElements(By.tagName("input"));
+        elements.get(0).click();
+        elements.get(1).click();
+    }
+
+    public void selectAndClickContextMenu(String id) {
+        actions.contextClick(driver.findElement(By.id(id))).perform();
+    }
+
+    public void dragAndDropElement(String idSource, String idTarget) {
+        WebElement source = driver.findElement(By.xpath(idSource));
+        WebElement target = driver.findElement(By.id(idTarget));
+        actions.dragAndDrop(source, target).build().perform();
+    }
+
+    public void selectDropdownMenu(String id) {
+        Select menu = new Select(driver.findElement(By.id(id)));
+        menu.selectByVisibleText("Option 1");
+        menu.selectByVisibleText("Option 2");
+    }
+
+
 }
